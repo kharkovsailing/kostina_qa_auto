7f9b5be0ca0afd726a8f5526f8965d59e146eec7
Olha Kostina <olgasail@gmail.com>
2024-02-14 13:24:24 -0800

    Added WebDriverWait() to test_ui_shop.py file

--- tests/ui/test_ui_shop.py
+++ tests/ui/test_ui_shop.py
@@ -6,7 +6,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.wait import WebDriverWait
-#from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions as EC
 
 
 items = 'product-item-link'
@@ -31,11 +31,12 @@ def test_check_first_element():
     select.select_by_value('price')
     
     # Чекаємо поки сторінка перезавантажиться
-    # Як вариант: WebDriverWait(driver, 10).until(EC.text_to_be_present_in_element((By.TAG_NAME, 'body'), 'Default welcome msg!'))
-    WebDriverWait(driver, 10)
+    # Як вариант: WebDriverWait(driver, 10)
+    WebDriverWait(driver, 10).until(EC.text_to_be_present_in_element((By.TAG_NAME, 'body'), 'Tees'))
+    
     
     # Знаходимо в 'product-item-link' назву першого товару
-    search_items = driver.find_elements(By.CLASS_NAME, 'product-item-link')
+    search_items = driver.find_elements(By.CLASS_NAME, items)
     new_first_item = search_items[0]
     
     
